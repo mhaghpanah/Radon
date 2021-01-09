a177003e4d7b2533dd468de8dc0407786d50c6f1
mhaghpanah <40441978+mhaghpanah@users.noreply.github.com>
2021-01-08 23:35:08 -0600

    Update CombinationEncodedIteratorTest.java

--- src/test/java/Radon/CombinationEncodedIteratorTest.java
+++ src/test/java/Radon/CombinationEncodedIteratorTest.java
@@ -2,13 +2,37 @@ package Radon;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Parameterized.class)
 public class CombinationEncodedIteratorTest {
 
-  public void helper(int n, int k, int sz) {
+  int n;
+  int k;
+  int sz;
+  public CombinationEncodedIteratorTest(int n, int k, int sz) {
+    this.n = n;
+    this.k = k;
+    this.sz = sz;
+  }
+
+  @Parameters
+  public static Collection<Object[]> data() {
+    return Arrays.asList(new Object[][]{
+        {1, 1, 1}, {1, 0, 1}, {5, 2, 10}, {5, 0, 1}, {5, 5, 1}, {0, 1, 0}, {7, 9, 0}, {-2, 0, 0},
+        {2, -1, 0}
+    });
+  }
+
+  @Test
+  public void test() {
     CombinationEncodedIterator it = new CombinationEncodedIterator(n, k);
     Set<Long> set = new HashSet<>();
     while (it.hasNext()) {
@@ -18,19 +42,6 @@ public class CombinationEncodedIteratorTest {
       set.add(v);
     }
     assertEquals(sz, set.size());
-  }
-
-  @Test
-  public void test() {
-    helper(1, 1, 1);
-    helper(1, 0, 1);
-    helper(5, 2, 10);
-    helper(5, 0, 1);
-    helper(5, 5, 1);
-    helper(0, 1, 0);
-    helper(7, 9, 0);
-    helper(-2, 0, 0);
-    helper(-2, -1, 0);
   }
 
 }
