9aac9f3d7291d26aa9f757dbe6d9cba3a61606ad
mhaghpanah <40441978+mhaghpanah@users.noreply.github.com>
2021-01-08 23:36:27 -0600

    Update CombinationIteratorTest.java

--- src/test/java/Radon/CombinationIteratorTest.java
+++ src/test/java/Radon/CombinationIteratorTest.java
@@ -4,12 +4,35 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Parameterized.class)
 public class CombinationIteratorTest {
+
+  int n;
+  int k;
+  int sz;
+  public CombinationIteratorTest(int n, int k, int sz) {
+    this.n = n;
+    this.k = k;
+    this.sz = sz;
+  }
+
+  @Parameters
+  public static Collection<Object[]> data() {
+    return Arrays.asList(new Object[][]{
+        {1, 1, 1}, {1, 0, 1}, {5, 2, 10}, {5, 0, 1}, {5, 5, 1}, {0, 1, 0}, {7, 9, 0}, {-2, 0, 0},
+        {2, -1, 0}
+    });
+  }
 
   public boolean isUnique(int[] nums) {
     Set<Integer> set = new HashSet<>();
@@ -40,7 +63,8 @@ public class CombinationIteratorTest {
     return ans;
   }
 
-  public void helper(int n, int k, int sz) {
+  @Test
+  public void test() {
     CombinationIterator it = new CombinationIterator(n, k);
     Set<List<Integer>> set = new HashSet<>();
     while (it.hasNext()) {
@@ -51,19 +75,6 @@ public class CombinationIteratorTest {
       set.add(toList(nums));
     }
     assertEquals(sz, set.size());
-  }
-
-  @Test
-  public void test() {
-    helper(1, 1, 1);
-    helper(1, 0, 1);
-    helper(5, 2, 10);
-    helper(5, 0, 1);
-    helper(5, 5, 1);
-    helper(0, 1, 0);
-    helper(7, 9, 0);
-    helper(-2, 0, 0);
-    helper(-2, -1, 0);
   }
 
 }
